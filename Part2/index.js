e302dcd3428e73456c0a13db7bcfafee046b73f0
trcasper <trcasper90@gmail.com>
2019-09-16 16:45:26 -0600

    Complete vanilla-js 1 afternoon

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+
+console.log("The house always wins")
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
